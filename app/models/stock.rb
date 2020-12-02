58af619812efb3cbec2229977fe6ab45f42fa113
Clément Hubert <clement@kirola.fr>
2020-12-03 00:17:19 +0100

    Use AJAX

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,11 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     StockQuote::Stock.new(api_key: Rails.application.credentials.iex_client[:api_tokens])
-    stock = StockQuote::Stock.quote(ticker_symbol)
-    stock.latest_price
+    begin
+      stock = StockQuote::Stock.quote(ticker_symbol)
+      new(ticker: ticker_symbol, name: stock.company_name, last_price: stock.latest_price)
+    rescue => exception
+      return nil
+    end
   end
 end
