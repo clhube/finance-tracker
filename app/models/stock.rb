ed1a6d8e58a93eb327be35f8b6bbbbfe15eae287
Clément Hubert <clement@kirola.fr>
2020-12-08 00:19:38 +0100

    Clean up Layout

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+
   def self.new_lookup(ticker_symbol)
     StockQuote::Stock.new(api_key: Rails.application.credentials.iex_client[:api_tokens])
     begin
