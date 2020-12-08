03692898605510274f8669ec00b8dcbc10ecc740
Clément Hubert <clement@kirola.fr>
2020-12-09 00:47:01 +0100

    Add Javascript response

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -13,7 +15,7 @@ class User < ApplicationRecord
     stocks.where(id: stock.id).exists?
   end
   
-         def under_stock_limit?
+  def under_stock_limit?
     stocks.count < 10
   end
   
@@ -25,5 +27,6 @@ class User < ApplicationRecord
     return "#{first_name} #{last_name}" if first_name || last_name
     "Anonymous"
   end
+  
 
 end
